a7d2ca4709f7e4c68201df5691b71af01e8fb17a
David-593 <andresdavidm759@gmail.com>
2025-09-05 19:11:51 -0500

    Agrego de cors para peticiones del frontend al backend

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const UsuarioRoute_1 = __importDefault(require("./routes/routeUsuario/UsuarioRoute"));
 const AuthRoute_1 = __importDefault(require("./routes/routeAuth/AuthRoute"));
 const AdminRoute_1 = __importDefault(require("./routes/routeAdmin/AdminRoute"));
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+app.use((0, cors_1.default)({
+    origin: "http://localhost:4200",
+    credentials: true
+}));
 app.use(express_1.default.json());
 app.use("/api/users", UsuarioRoute_1.default);
 app.use("/api/auth", AuthRoute_1.default);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"; 
 import usuarioRoutes from "./routes/routeUsuario/UsuarioRoute";
 import authRoute from "./routes/routeAuth/AuthRoute";
 import adminRoute from "./routes/routeAdmin/AdminRoute";
@@ -6,11 +7,16 @@ import adminRoute from "./routes/routeAdmin/AdminRoute";
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors({
+  origin: "http://localhost:4200",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/users", usuarioRoutes);
-app.use("/api/auth", authRoute)
-app.use("/api/admin", adminRoute); 
+app.use("/api/auth", authRoute);
+app.use("/api/admin", adminRoute);
 
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
